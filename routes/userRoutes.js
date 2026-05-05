13ff939b5aa56af2672eed69326bde93bb68ae00
Anuj Dubey <anujdubey.ok@gmail.com>
2026-05-06 01:24:14 +0530

    feat: add authentication middleware to protect user routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,8 +2,9 @@ const express = require('express');
 const { get } = require('mongoose');
 const router = express.Router();
 const { getUserController } = require('../controllers/userController.js');  
+const authMiddleware = require('../middleware/authMiddleware.js');
 
 //routes
-router.get('/getUser', getUserController);
+router.get('/getUser', authMiddleware, getUserController);
 // Register 
 module.exports = router;
